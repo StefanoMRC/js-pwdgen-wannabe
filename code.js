434a716f1887d174bb02b7f71ecc5c2c3c0efce3
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-08 14:07:58 +0100

    fine esercizio

---
+++ code.js
@@ -0,0 +1,12 @@
+let nome = prompt("qual è il tuo nome?");
+let cognome = prompt ("qual è il tuo cognome?");
+let colore = prompt ("qual è il tuo colore preferito?");
+let numero = 12;
+
+console.log(nome + cognome + colore + numero);
+
+let frase = (nome + cognome + colore + numero);
+
+console.log=(frase);
+
+document.getElementById("cont_pass").innerHTML = frase;
